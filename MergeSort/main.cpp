34aca248007293f0f12261abf423f769a6021f9e
Mark Barho <zazazasaza@mail.ru>
2018-01-20 21:03:08 +0200

    Templates support implemented

--- MergeSort/main.cpp
+++ MergeSort/main.cpp
@@ -5,8 +5,9 @@ using namespace std;
  * first sorted fragment is [start1, start2) and second is [start2, end)
  * they are merged in growing order and stored in [start1, end)
  */
-void mergeFragments(vector<int> &src, int start1, int start2, int end){
-    vector<int> sortedFragments(end - start1);
+template <class MyClass>
+void mergeFragments(vector<MyClass> &src, int start1, int start2, int end, bool less(MyClass, MyClass)){
+    vector<MyClass> sortedFragments(end - start1);
     for(int i=0, first=start1, second=start2; i<sortedFragments.size(); i++){
         if(first==start2){
             sortedFragments[i] = src[second];
@@ -14,7 +15,7 @@ void mergeFragments(vector<int> &src, int start1, int start2, int end){
         } else if (second == end){
             sortedFragments[i] = src[first];
             first++;
-        } else if(src[first] < src[second]){
+        } else if(less(src[first], src[second])){
             sortedFragments[i] = src[first];
             first++;
         } else {
@@ -26,12 +27,13 @@ void mergeFragments(vector<int> &src, int start1, int start2, int end){
         src[j] = sortedFragments[i];
 }
 
-void mergeSort(vector<int> &src){
+template <class MyClass>
+void mergeSort(vector<MyClass> &src, bool less(MyClass, MyClass)){
     for(int step = 1; step < src.size(); step*=2){
         for(int i=0; i<src.size(); i+=step*2){
             int j = min(i+step, (int)src.size());
             int end = min(j+step, (int)src.size());
-            mergeFragments(src, i, j, end);
+            mergeFragments(src, i, j, end, less);
         }
     }
 }
@@ -42,7 +44,7 @@ void testMergeSort(){
         randomData[i] = rand()%5000;
     vector<int> copyOfRandomData(randomData);
     sort(copyOfRandomData.begin(), copyOfRandomData.end());
-    mergeSort(randomData);
+    mergeSort(randomData, [](int a, int b){return a<b;});
     for(int i=0; i<randomData.size(); i++){
         if(randomData[i] != copyOfRandomData[i]){
             cout<<"Merge sort:\n";
